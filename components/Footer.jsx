4781571152c585202ca0f1db31fe9d262774fd5c
Ammar.yasir <m.ammar@bondwest.co.uk>
2024-09-19 00:50:53 +0500

    flex-between class added in footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -43,7 +43,7 @@ const Footer = () => (
             </div>
         </div>
 
-        <div className="flexBetween footer_copyright">
+        <div className="flex-between footer_copyright">
             <p>@ 2023 Promptopia. All rights reserved</p>
             <p className="text-gray">
                 <span className="text-black font-semibold">10,214</span> projects submitted
