ebd92c074e7cde11d93f4ee01aaf06d377a5fe76
Ammar.yasir <m.ammar@bondwest.co.uk>
2024-09-19 15:35:54 +0500

    profile page edited

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,27 +1,24 @@
-// components/Profile.jsx
 import PromptCard from './PromptCard';
 
-const Profile = ({ name, desc, data }) => {
+const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
   return (
-    <div className="profile">
-      <h1>{name}</h1>
-      <p>{desc}</p>
-      {/* Display posts */}
-      <div className="posts">
-        {data.length > 0 ? (
-          data.map(post => (
-            <PromptCard
-              key={post._id}
-              post={post}
-              // Add any other necessary props for PromptCard
-            />
-          ))
-        ) : (
-          <p>No posts available.</p>
-        )}
-      </div>
+    <section className="w-full;">
+      <h1 className="head_text text-left">
+        <span className="blue_gradient">{name} Profile</span>
+      </h1>
+      <p className="desc text-left">{desc}</p>
+      <div className="mt-10 prompt_layout">
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleEdit={() => handleEdit && handleEdit(post)}
+          handleDelete={() => handleDelete && handleEdit(post)}
+        />
+      ))}
     </div>
-  );
-};
+    </section>
+  )
+}
 
-export default Profile;
+export default Profile
